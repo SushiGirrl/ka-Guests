165639caa0ffdd879231277bddea5101849e45b9
SushiGirrl <lykkeflor@gmail.com>
2024-10-30 10:44:22 +0100

    added app-db to gitignore

--- main.py
+++ main.py
@@ -67,6 +67,5 @@ def delete_guest(guest_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
 
